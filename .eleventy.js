f9d2512f4f488b5fce95d05044b08b8b1d734379
methias <tmn5@pm.me>
2020-08-08 20:08:33 +0300

    Pass through images in eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = (config) => {
+  // Set directories to pass through to the dist folder
+  config.addPassthroughCopy('./src/images/')
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
