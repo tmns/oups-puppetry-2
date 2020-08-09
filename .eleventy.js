4b1343deb134adb56c9dbae97d11f2f609777716
methias <tmn5@pm.me>
2020-08-09 12:28:15 +0300

    Add people collection data & config setting

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,13 @@ module.exports = (config) => {
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy('./src/images/')
 
+  // Returns a list of people ordered by filename
+  config.addCollection('people', (collection) =>
+    collection
+      .getFilteredByGlob('./src/people/*.md')
+      .sort((a, b) => (Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1))
+  )
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
