fe00f36a57cbf8818d87d14b0b11799d48a1c2f7
methias <tmn5@pm.me>
2020-09-03 20:50:21 +0300

    Add html-min-transform

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
 const dateFilter = require('./src/filters/date-filter.js')
 const w3DateFilter = require('./src/filters/w3-date-filter.js')
+const htmlMinTransform = require('./src/transforms/html-min-transform')
+
+const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = (config) => {
-  // Set directories to pass through to the dist folder
-  config.addPassthroughCopy('./src/images/')
+  // Minify html, inline css, & inline js if we are in prod
+  if (isProduction) {
+    config.addTransform('htmlmin', htmlMinTransform)
+  }
 
   // Returns a list of people ordered by filename
   config.addCollection('people', (collection) =>
@@ -14,56 +19,61 @@ module.exports = (config) => {
 
   // Returns a list of shows ordered by filename
   config.addCollection('shows', (collection) =>
-    collection.getFilteredByGlob('./src/shows/*.md')
+    collection
+      .getFilteredByGlob('./src/shows/*.md')
       .sort((a, b) => (Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1))
   )
 
   // Returns a list of workshops ordered by filename
   config.addCollection('workshops', (collection) =>
-    collection.getFilteredByGlob('./src/workshops/*.md')
+    collection
+      .getFilteredByGlob('./src/workshops/*.md')
       .sort((a, b) => (Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1))
   )
 
   // Returns a list of seminars ordered by filename
   config.addCollection('seminars', (collection) =>
-    collection.getFilteredByGlob('./src/seminars/*.md')
+    collection
+      .getFilteredByGlob('./src/seminars/*.md')
       .sort((a, b) => (Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1))
   )
 
   // Returns a list of tour dates ordered by filename
   config.addCollection('tourDates', (collection) =>
-    collection.getFilteredByGlob('./src/tour-dates/*.md')
+    collection
+      .getFilteredByGlob('./src/tour-dates/*.md')
       .sort((a, b) => (Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1))
   )
 
   // Returns all placeholders
   config.addCollection('placeholders', (collection) =>
-    collection.getFilteredByGlob('./src/placeholders/*.md'))
+    collection.getFilteredByGlob('./src/placeholders/*.md')
+  )
 
   // Add filters
   config.addFilter('dateFilter', dateFilter)
   config.addFilter('w3DateFilter', w3DateFilter)
 
   // Filter that helps debugging potentially circular objects
-  config.addFilter('dump', obj => {
+  config.addFilter('dump', (obj) => {
     const getCircularReplacer = () => {
-      const seen = new WeakSet();
+      const seen = new WeakSet()
       return (key, value) => {
-        if (typeof value === "object" && value !== null) {
+        if (typeof value === 'object' && value !== null) {
           if (seen.has(value)) {
-            return;
+            return
           }
-          seen.add(value);
+          seen.add(value)
         }
-        return value;
-      };
-    };
-  
-    return JSON.stringify(obj, getCircularReplacer(), 4);
-  });
+        return value
+      }
+    }
+
+    return JSON.stringify(obj, getCircularReplacer(), 4)
+  })
 
   // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
-  config.setUseGitIgnore(false);
+  config.setUseGitIgnore(false)
 
   return {
     markdownTemplateEngine: 'njk',
---
+++ src/transforms/html-min-transform.js
@@ -0,0 +1,15 @@
+const htmlmin = require('html-minifier')
+
+module.exports = (value, outputPath) => {
+  if (outputPath && outputPath.indexOf('.html') > -1) {
+    return htmlmin.minify(value, {
+      useShortDoctype: true,
+      removeComments: true,
+      collapseWhitespace: true,
+      minifyCSS: true,
+      jsmin: true,
+    })
+  }
+
+  return value
+}
